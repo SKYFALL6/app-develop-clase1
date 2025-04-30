3aebea9d32414f8ff3fadf9385d1860bb2b66e35
SKYFALL6 <danialexi2019@gmail.com>
2025-04-29 20:33:03 -0500

    clase 2

--- JS/loginService.js
+++ JS/loginService.js
@@ -3,25 +3,57 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     alert('Hola estos son tus datos: email: ' + email + 'pass: ' + password)
+    login(email, password)
+})
 
-    let mensaje = ''
-    let tipoAlerta = ''
-    if (email === '' || password === '') {
-        mensaje = 'Por favor completa todos los campos.';
-        tipoAlerta = 'warning'
-    }
-    else if (email === 'prueba@gmail.com' && password === '123456') {
-        mensaje = 'Inicio de sesion exitoso.'
-        tipoAlerta = 'success'
-    }
-    else {
-        mensaje = 'Correo o contraseña incorrectos'
-        tipoAlerta = 'danger'
-    }
-    let alerta = `
-        <div class="alert alert-${tipoAlerta} alert-dismissible fade show" role="alert">
-        ${mensaje}
+
+function login(email, password) {
+    localStorage.removeItem('token')
+    let message = ''
+    let alertType = ''
+    const REQRES_ENPOINT = 'https://reqres.in/api/login'
+    fetch(REQRES_ENPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'x-api-key': 'reqres-free-v1'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then((response) => {
+            if (response.status == 200) {
+                alertType = 'success'
+                message = 'Inicio de sesión exitoso'
+                alertBuilder(alertType, message)
+                localStorage.setItem('token', 'qwertyuiopasdfghjkl')
+                setTimeout(() => {
+                    location.href = 'admin/Dashboard.html';
+
+                }, 2000)//2000ms = 2s
+            }
+            else {
+                alertType = 'danger'
+                message = 'correo o contraseña invalida'
+                alertBuilder(alertType, message)
+            }
+            console.log('respuesta de servicio', response)
+            
+        })
+        .catch((error) => {
+            alertType = 'danger'
+            message = 'Correo o contraseña invalida'
+            console.log('error en el servicio', error)
+            alertBuilder(alertType, message)
+        })
+
+
+}
+
+function alertBuilder(alertType, message) {
+    const alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+        ${message}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>`;
-    document.getElementById('mensaje').innerHTML = alerta;
-})
+    document.getElementById('mensaje').innerHTML = alert;
+}
