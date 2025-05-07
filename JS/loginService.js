5ff9f2c07f67cabf04671283c0edbe8869432ece
SKYFALL6 <danialexi2019@gmail.com>
2025-05-06 21:16:07 -0500

    clase 3

--- JS/loginService.js
+++ JS/loginService.js
@@ -1,59 +1,56 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    alert('Hola estos son tus datos: email: ' + email + 'pass: ' + password)
-    login(email, password)
-})
+document.getElementById('loginForm').addEventListener('submit', function(e){
+	e.preventDefault();
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	login(email, password)
+});
 
-
-function login(email, password) {
-    localStorage.removeItem('token')
-    let message = ''
-    let alertType = ''
-    const REQRES_ENPOINT = 'https://reqres.in/api/login'
-    fetch(REQRES_ENPOINT, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-            'x-api-key': 'reqres-free-v1'
-        },
-        body: JSON.stringify({ email, password })
-    })
-        .then((response) => {
-            if (response.status == 200) {
-                alertType = 'success'
-                message = 'Inicio de sesión exitoso'
-                alertBuilder(alertType, message)
-                localStorage.setItem('token', 'qwertyuiopasdfghjkl')
-                setTimeout(() => {
-                    location.href = 'admin/Dashboard.html';
-
-                }, 2000)//2000ms = 2s
-            }
-            else {
-                alertType = 'danger'
-                message = 'correo o contraseña invalida'
-                alertBuilder(alertType, message)
-            }
-            console.log('respuesta de servicio', response)
-            
-        })
-        .catch((error) => {
-            alertType = 'danger'
-            message = 'Correo o contraseña invalida'
-            console.log('error en el servicio', error)
-            alertBuilder(alertType, message)
-        })
-
-
+function login(email, password){
+	localStorage.removeItem('token')
+	let message = ''
+	let alertType = ''
+	const REQRES_ENDPOINT = 'https://reqres.in/api/login'
+	fetch(REQRES_ENDPOINT, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'x-api-key': 'reqres-free-v1'
+		},
+		body: JSON.stringify({email, password})
+	})
+	.then((response) => {
+		if(response.status === 200){
+			alertType = 'success'
+			message = 'inicio de sesion exitoso'
+			alertBuilder(alertType, message)
+			localStorage.setItem('token', 'dbhjbdjsdnasnssg')
+			setTimeout(() => {
+				location.href = 'admin/dashboard.html';
+			}, 2000)
+			
+		}
+		else{
+			alertType = 'danger'
+			message = 'inicio de sesion invalida'
+			alertBuilder(alertType, message)
+		}
+		console.log('respuesta del servicio', response)
+		
+	})
+	.catch((error) =>{
+		alertType = 'danger'
+		message = 'ocurrio un error inesperado'
+		console.log('error en el servicio', error)
+		alertBuilder(alertType, message)
+	})
+	
 }
 
-function alertBuilder(alertType, message) {
-    const alert = `
-        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-        ${message}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>`;
-    document.getElementById('mensaje').innerHTML = alert;
+function alertBuilder(alertType, message){
+	const alert =
+	 `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+		${message}
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>`;
+	document.getElementById('mensaje').innerHTML = alert;
 }
